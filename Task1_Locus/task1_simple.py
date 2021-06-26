f1c1bee1afa217a988829386a030e6a59177918c
mathurprateek <37469411+mathurprateek@users.noreply.github.com>
2021-06-26 18:31:51 +0530

    Update task1_simple.py
    Added line comments for understanding code

--- Task1_Locus/task1_simple.py
+++ Task1_Locus/task1_simple.py
@@ -14,11 +14,11 @@ unix_timestamp = int((dt(dt.now(tz).year, dt.now(tz).month, dt.now(tz).day, 4, t
 output_dict = {}  # dictionary to store timestamp and pressure values
 
 for i in range(1, 4):
-    tm = unix_timestamp - (86400*i)
+    tm = unix_timestamp - (86400*i)  # evaluating timestamp for last three days
     res = requests.get('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={}&lon={}&dt={}'
-                       '&appid=78385f931961ae9b80060c70d2c7bf6f'.format(lat, lon, tm))
+                       '&appid=78385f931961ae9b80060c70d2c7bf6f'.format(lat, lon, tm))  # API calling
     output_dict[str(res.json()['current']['dt']) + ' ' + 'Unix Time'] \
-        = str(res.json()['current']['pressure']) + ' ' + 'Millibar'
+        = str(res.json()['current']['pressure']) + ' ' + 'Millibar'  # store response in dictionary
 
 print("Atmospheric pressure (Millibar) at 4:00am (Unix Time) for the past 3 days")
 print(json.dumps(output_dict, indent=4))
